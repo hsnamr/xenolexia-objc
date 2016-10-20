c98364666ce83417e9970fb32fddb58c1e7ec188
Hussian Alamri <husain.amri@me.com>
2016-10-20 15:06:30 -0500

    Update HtmlToTextService.m

--- HtmlToTextService.m
+++ HtmlToTextService.m
@@ -2,9 +2,6 @@
 //  HtmlToTextService.m
 //  DownloadAndDisplayLocalHTML
 //
-//  Created by admin on 5/21/16.
-//  Copyright © 2016 BrighterBrain. All rights reserved.
-//
 
 #import "HtmlToTextService.h"
 
